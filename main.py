b8ed1a4dd9c4e2e928eaf4cefe6bf5b30ad69000
Felipe <fhora93@gmail.com>
2024-07-30 18:49:47 -0300

    Data Cleaning and Converting types

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ df_desemprego = pd.read_csv("Google+Trends+Data+Viz+(start)\\UE Benefits Search 
 btc_price = pd.read_csv("Google+Trends+Data+Viz+(start)\Daily Bitcoin Price.csv")
 btc_search = pd.read_csv("Google+Trends+Data+Viz+(start)\Bitcoin Search Trend.csv")
 
-# print(df_desemprego.info())
-# print(df_tesla.info())
-# print(btc_price.info())
-# print(btc_search.info())
+print(df_desemprego.info())
+# print(df_tesla.describe())
+# print(btc_price.describe())
+# print(btc_search.describe())
 
-#SIMPLE DATA EXPLORATION
+#DATA EXPLORATION
 # max_value_tesla = df_tesla["TSLA_USD_CLOSE"].max()
 # min_value_tesla = df_tesla["TSLA_USD_CLOSE"].min()
 # max_value_des = df_desemprego["UE_BENEFITS_WEB_SEARCH"].max()
@@ -19,9 +19,20 @@ btc_search = pd.read_csv("Google+Trends+Data+Viz+(start)\Bitcoin Search Trend.cs
 # print('Largest value for "Unemployemnt Benefits" Web Search: ', max_value_des)
 # What does a value of 100 in the Google Trend search popularity actually mean?
 # The Google Trends data is normalized to a scale of 0 to 100, where 100 is the maximun value
-print(f'Missing values for Tesla?: {df_tesla.isna().values.any()}')
-print(f'Missing values for U/E?: {df_desemprego.isna().values.any()}')
-print(f'Missing values for BTC Search?: {btc_search.isna().values.any()}')
-print(f'Missing values for BTC Search?: {btc_price.isna().values.any()}')
-print(f'Missing values for BTC Search?: {btc_price.isna().values.any().sum()}')
+# print(f'Missing values for Tesla?: {df_tesla.isna().values.any()}')
+# print(f'Missing values for U/E?: {df_desemprego.isna().values.any()}')
+# print(f'Missing values for BTC Search?: {btc_search.isna().values.any()}')
+# print(f'Missing values for BTC Search?: {btc_price.isna().values.any()}')
+# print(f'Missing values for BTC Search?: {btc_price.isna().values.any().sum()}')
+# Only one value, so it's better just drop this index
+
 #DATA CLEANING
+# Removing the index with no value
+btc_price.dropna(inplace=True)
+
+# Converting Strings to DateTime Objects
+df_tesla.MONTH = pd.to_datetime(df_tesla.MONTH)
+df_desemprego.MONTH = pd.to_datetime(df_desemprego.MONTH)
+btc_search.MONTH = pd.to_datetime(btc_search.MONTH)
+btc_price.DATE = pd.to_datetime(btc_price.DATE)
+print(df_desemprego.info()) #Confirming the new type
